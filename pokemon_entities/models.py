219067f66b104178a2b39b9d8c7c9122bb22df35
dntumanov <dtumanov@sfedu.ru>
2021-03-27 10:20:06 +0300

    (project)  start 4th lesson

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# your models here
+
+class Pokemon(models.Model):
+    """Описание сущности покенома"""
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='pokemons', blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    appeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField()
+
