734d0946e51a61936d8029cd835c8be6e8adcab6
dntumanov <dtumanov@sfedu.ru>
2021-04-22 14:47:38 +0300

    style(project) add flake8 and refactor code

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,14 +3,21 @@ from django.db import models
 
 class Pokemon(models.Model):
     """Описание покенома"""
-    title_ru = models.CharField(max_length=200, verbose_name='Имя покемона на русском', default='покемон')
-    title_en = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на английском')
-    title_jp = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на японском')
-    image = models.ImageField(upload_to='pokemons', null=True, blank=True, verbose_name='Ваставьте картинку для покемона')
+    title_ru = models.CharField(
+        max_length=200, verbose_name='Имя покемона на русском',
+        default='покемон')
+    title_en = models.CharField(
+        max_length=200, blank=True, verbose_name='Имя покемона на английском')
+    title_jp = models.CharField(
+        max_length=200, blank=True, verbose_name='Имя покемона на японском')
+    image = models.ImageField(
+        upload_to='pokemons', null=True, blank=True,
+        verbose_name='Ваставьте картинку для покемона')
     description = models.TextField(
         verbose_name='Описание покемона', blank=True)
-    previous_evolution = models.ForeignKey('Pokemon', null=True, on_delete=models.SET_NULL,
-                                           related_name='next_evolution', verbose_name='Из кого эволюционировал')
+    previous_evolution = models.ForeignKey(
+        'Pokemon', null=True, on_delete=models.SET_NULL,
+        related_name='next_evolution', verbose_name='Из кого эволюционировал')
 
     @property
     def get_image_absolute_url(self):
@@ -25,13 +32,19 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     """Описание свойст покемона"""
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(
+        Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
     lat = models.FloatField(verbose_name='Координаты покемона: широта')
     lon = models.FloatField(verbose_name='Координаты покемона: долгота')
     appeared_at = models.DateTimeField('Появился в')
     disappeared_at = models.DateTimeField('Исчез в')
-    level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True,  blank=True, verbose_name='Здоровье')
-    strength = models.IntegerField(null=True,  blank=True, verbose_name='Показатель атаки')
-    defence = models.IntegerField(null=True,  blank=True, verbose_name='Показатель защиты')
-    stamina = models.IntegerField(null=True,  blank=True, verbose_name='Показатель выносливости')
+    level = models.IntegerField(
+        null=True, blank=True, verbose_name='Уровень')
+    health = models.IntegerField(
+        null=True, blank=True, verbose_name='Здоровье')
+    strength = models.IntegerField(
+        null=True, blank=True, verbose_name='Показатель атаки')
+    defence = models.IntegerField(
+        null=True, blank=True, verbose_name='Показатель защиты')
+    stamina = models.IntegerField(
+        null=True, blank=True, verbose_name='Показатель выносливости')
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render, get_object_or_404
 from pokemon_entities.models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
-DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
+DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/" \
+                    "6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/" \
+                    "width/240/height/240?cb=20130525215832&fill=transparent"
 
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
@@ -16,7 +18,8 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
     )
     folium.Marker(
         [lat, lon],
-        # Warning! `tooltip` attribute is disabled intentionally to fix strange folium cyrillic encoding bug
+        # Warning! `tooltip` attribute is disabled intentionally
+        # to fix strange folium cyrillic encoding bug
         icon=icon,
     ).add_to(folium_map)
 
@@ -28,7 +31,8 @@ def show_all_pokemons(request):
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_enity in pokemons_enity:
-        image_path = request.build_absolute_uri(pokemon_enity.pokemon.get_image_absolute_url)
+        image_path = request.build_absolute_uri(
+            pokemon_enity.pokemon.get_image_absolute_url)
         add_pokemon(
             folium_map, pokemon_enity.lat,
             pokemon_enity.lon,
@@ -53,7 +57,8 @@ def show_pokemon(request, pokemon_id):
         'title_en': pokemon.title_en,
         'title_jp': pokemon.title_jp,
     }
-    image_path = request.build_absolute_uri(pokemon_enity.pokemon.get_image_absolute_url)
+    image_path = request.build_absolute_uri(
+        pokemon_enity.pokemon.get_image_absolute_url)
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
     add_pokemon(
@@ -78,5 +83,8 @@ def show_pokemon(request, pokemon_id):
             'title_ru': pokemon.title_ru
         }
 
-    return render(request, "pokemon.html", context={'map': folium_map._repr_html_(),
-                                                    'pokemon': pokemon_view_on_page})
+    return render(
+        request, "pokemon.html",
+        context={
+            'map': folium_map._repr_html_(),
+            'pokemon': pokemon_view_on_page})
